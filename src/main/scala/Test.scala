8b016749358d342d8c7aa9040617aeeda4c987a4
hyg <hyg@webterren.com>
2018-05-18 02:08:31 +0800

    尝试用Jsoup，好丑的代码

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,8 +1,12 @@
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
+
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import org.jsoup.Jsoup
 import play.api.libs.ws.ahc._
-import scala.concurrent._
-import ExecutionContext.Implicits.global
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 
 object Main extends App {
@@ -10,16 +14,30 @@ object Main extends App {
   implicit val mat: akka.stream.Materializer = ActorMaterializer()
 
 
+  def detectAndGetBody(url: String, body: String, contentType: String): String = {
+    val is = new ByteArrayInputStream(body.getBytes(StandardCharsets.ISO_8859_1))
+    val idx = contentType.indexOf("charset=")
+    var charset = "UTF-8"
+    if (idx > 0) {
+      charset = contentType.substring(idx + 8)
+    }
+    val doc = Jsoup.parse(is, charset, url);
+    is.close()
+    return doc.body().html()
+  }
+
   override def main(args: Array[String]): Unit = {
     val wsClient = AhcWSClient()
 
-    wsClient.url("http://www.sina.com.cn/")
-      .addHttpHeaders("User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36" )
+    val url = "http://www.sina.com.cn/"
+
+    wsClient.url(url)
+      .addHttpHeaders("User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36")
       .get()
-      .map (
+      .map(
         response => {
           response.headers.foreach(println)
-          println(response.body)
+          println(detectAndGetBody(url, response.body, response.header("Content-Type").getOrElse("")))
         }
       )
   }
